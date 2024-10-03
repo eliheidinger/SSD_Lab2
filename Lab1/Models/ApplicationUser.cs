6de04225c50b6778c83b4f2fc7742eb4b8524fcc
Eli <sense@ELISLAPTOP>
2024-10-03 15:16:23 -0400

    Add project files.

---
+++ Lab1/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Birth Date")]
+        public DateTime? BirthDate { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public override string? PhoneNumber { get; set; } //override so phone num can be nullable
+    }
+}
---
+++ Lab1/Models/Employer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Lab1.Models
+{
+    public class Employer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Employer Name is required.")]
+        [Display(Name = "Employer Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required.")]
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Website is required.")]
+        [Url(ErrorMessage = "Invalid website URL.")]
+        public string Website { get; set; }
+
+        [Display(Name = "Incorporated Date")]
+        public DateTime? IncorporatedDate { get; set; }
+    }
+}
