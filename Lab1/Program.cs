71c04ed992e1d27cf60742fd6c7ac0966b70fd7c
Eli <sense@ELISLAPTOP>
2024-10-06 21:19:15 -0400

    finally working :)

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,6 +1,3 @@
-using Azure;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using Lab1.Data;
 using Lab1.Models;
 using Microsoft.AspNetCore.Identity;
@@ -10,15 +7,13 @@ namespace Lab1
 {
     public class Program
     {
-        public static string ManagerPassword;
-        public static string EmployeePassword;
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container with the secret connection string
+            // Add services to the container with the connection string from appsettings.json
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -26,33 +21,38 @@ namespace Lab1
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
-            var kvUri = new Uri(builder.Configuration.GetSection("KVURI").Value);
-            var azCred = new DefaultAzureCredential();
+            //trying to user key vault in older version of azure, couldnt work at all
+            //var kvUri = new Uri(builder.Configuration.GetSection("KVURI").Value);
 
-            try
-            {
-                builder.Configuration.AddAzureKeyVault(kvUri, azCred);
-                ManagerPassword = builder.Configuration["ManagerPassword"];
-                EmployeePassword = builder.Configuration["EmployeePassword"];
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error retrieving secrets from Key Vault: {ex.Message}");
-            }
+            //var azCred = new DefaultAzureCredential();
+
+            //try
+            //{
+            //    builder.Configuration.AddAzureKeyVault(kvUri, azCred);
+            //    ManagerPassword = builder.Configuration["ManagerPassword"];
+            //    EmployeePassword = builder.Configuration["EmployeePassword"];
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Error retrieving secrets from Key Vault: {ex.Message}");
+            //}
 
             //ManagerPassword = "ManagerPassword1!";
             //EmployeePassword = "EmployeePassword1!";
 
             var app = builder.Build();
 
+            // Bind configuration from appsettings.json
+            var configuration = app.Services.GetService<IConfiguration>();
+            var secrets = configuration.GetSection("Secrets").Get<AppSecrets>();
+
             // Seed roles and users
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
-                SeedRolesAndUsersAsync(userManager, roleManager).Wait();
+                // Pass secrets to DbInitializer
+                DbInitializer.SeedUsersAndRoles(services, secrets).Wait();
             }
 
             // Configure the HTTP request pipeline.
@@ -82,57 +82,6 @@ namespace Lab1
             app.Run();
         }
 
-        // Method to seed roles and users asynchronously
-        private static async Task SeedRolesAndUsersAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
-        {
-            
-
-            var roles = new[] { "Manager", "Employee" };
-            foreach (var role in roles)
-            {
-                if (!await roleManager.RoleExistsAsync(role))
-                {
-                    await roleManager.CreateAsync(new IdentityRole(role));
-                }
-            }
-
-            // Create Manager user
-            var managerUser = new ApplicationUser
-            {
-                UserName = "manager@example.com",
-                Email = "manager@example.com",
-                FirstName = "ManagerFirstName",
-                LastName = "ManagerLastName",
-                PhoneNumber = "1234567890",
-                EmailConfirmed = true
-            };
-            if (userManager.Users.All(u => u.UserName != managerUser.UserName))
-            {
-                var result = await userManager.CreateAsync(managerUser, ManagerPassword);
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(managerUser, "Manager");
-                }
-            }
-
-            // Create Employee user
-            var employeeUser = new ApplicationUser
-            {
-                UserName = "employee@example.com",
-                Email = "employee@example.com",
-                FirstName = "EmployeeFirstName",
-                LastName = "EmployeeLastName",
-                PhoneNumber = "0987654321",
-                EmailConfirmed = true
-            };
-            if (userManager.Users.All(u => u.UserName != employeeUser.UserName))
-            {
-                var result = await userManager.CreateAsync(employeeUser, EmployeePassword);
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(employeeUser, "Employee");
-                }
-            }
-        }
+       
     }
 }
